0fa5ef222afab00012997c83d8ed7df3075d534b
bdj <bdj@pr0xy.co>
2025-08-20 11:07:36 -0700

    Payment error msg: guide users to retry

--- packages/atxp-common/src/paymentRequiredError.ts
+++ packages/atxp-common/src/paymentRequiredError.ts
@@ -14,5 +14,5 @@ export function paymentRequiredError(server: AuthorizationServerUrl, paymentRequ
   const paymentRequestUrl = `${server}/payment-request/${paymentRequestId}`;
   const data = { paymentRequestId, paymentRequestUrl, chargeAmount };
   const amountText = chargeAmount ? ` You will be charged ${chargeAmount.toString()}.` : '';
-  return new McpError(PAYMENT_REQUIRED_ERROR_CODE, `${PAYMENT_REQUIRED_PREAMBLE}${amountText} Please pay at: ${paymentRequestUrl}`, data);
+  return new McpError(PAYMENT_REQUIRED_ERROR_CODE, `${PAYMENT_REQUIRED_PREAMBLE}${amountText} Please pay at: ${paymentRequestUrl} and then try again.`, data);
 } 
