8b857b0e57a234765f04f25e75ae496cb5a386ff
Brian DeJong <bdj@pr0xy.co>
2025-10-06 09:40:12 -0700

    Fix build issues (#88)

--- packages/atxp-x402/rollup.config.js
+++ packages/atxp-x402/rollup.config.js
@@ -4,6 +4,7 @@ export default createConfig('atxp-x402', {
   platform: 'neutral', // Browser/React Native
   external: [
     // x402 and its dependencies
-    'x402'
+    'x402',
+    'x402/client'
   ]
 });
